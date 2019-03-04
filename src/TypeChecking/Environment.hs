d2ce9afc98f7929ec2ede5eaeded6e7998e16abc
LUA Ting-Gan <banacorn@gmail.com>
2019-03-04 17:09:52 +0800

    rewrite the type inferer from scratch

--- src/TypeChecking/Environment.hs
+++ src/TypeChecking/Environment.hs
@@ -1,72 +1,65 @@
 module TypeChecking.Environment where
 
--- import qualified Syntax.Concrete as C
-import Syntax.Concrete
+import qualified Syntax.Concrete as C
+import Syntax.Concrete (HasDual(..))
+import Syntax.TypeChecking
 
 import Prelude hiding (lookup)
 import Control.Monad
 import Control.Monad.State
 import Control.Monad.Except
 
-import Data.Function (on)
 import Data.Loc (Loc)
 import Data.Map (Map)
 import qualified Data.Map as Map
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Text (Text)
+import qualified Data.Map.Merge.Lazy as Map
+-- import Data.Set (Set)
+-- import qualified Data.Set as Set
+-- import Data.Text (Text)
 
 --------------------------------------------------------------------------------
 -- | Monad & Types
 
-type Var = TermName Loc
+type Var = C.TermName Loc
+type Term = C.Process Loc
 
-data Index = Pos Int | Neg Int
-    deriving (Show)
-
-instance HasDual Index where
-  dual (Pos i) = Neg i
-  dual (Neg i) = Pos i
-
-instance Eq Index where
-  (==) = (==) `on` dual
-
--- basically just Types
-data Prop = Concrete (Type Loc) -- concrete types
-          | Abstract Index      -- type varialbes with Index as names
-  deriving (Show)
-
-type Term = Process Loc
-type Context = Map Var Prop
+-- Γ, Δ, bunch of Var - Type pair
+type Context = Map Var Type
 
 data EnvState = EnvState
-  { stContext  :: Context
-  , stVarCount :: Int
+  { stContexts  :: Map Int Context  -- bunch of contexts
+  , stCtxCount  :: Int              -- for context variables
+  , stTypes     :: Context          -- bunch of types
+  , stTypeCount :: Int              -- for type variables
   } deriving (Show)
 
-initialEnvState :: EnvState
-initialEnvState = EnvState Map.empty 0
-
+-- initialEnvState :: EnvState
+-- initialEnvState = EnvState Map.empty 0
+--
 data InferError
   = General String
-  | NotInContext Var
-  | CannotUnify (Type Loc) (Type Loc)
-  | VarNotFresh Var (Maybe Term)
+  | VarNotFoundInTypes Var
+  | OverlappedContext Context
+  | CannotUnify Type Type
+--   | VarNotFresh Var (Maybe Term)
   deriving (Show)
-
+--
 type EnvM = ExceptT InferError (State EnvState)
 
-putContext :: Context -> EnvM ()
-putContext x = modify $ \ st -> st { stContext = x }
+putTypes :: Context -> EnvM ()
+putTypes x = modify $ \ st -> st { stTypes = x }
 
-getFreshTypeVar :: EnvM Prop
-getFreshTypeVar = do
-  i <- gets stVarCount
-  modify $ \ st -> st { stVarCount = i + 1 }
-  return $ Abstract (Pos i)
+putContexts :: Map Int Context -> EnvM ()
+putContexts x = modify $ \ st -> st { stContexts = x }
 
-addToContext :: Var -> Prop -> EnvM ()
-addToContext var prop = modify $ \ st -> st { stContext = Map.insert var prop (stContext st) }
+-- getFreshTypeVar :: EnvM Prop
+-- getFreshTypeVar = do
+--   i <- gets stVarCount
+--   modify $ \ st -> st { stVarCount = i + 1 }
+--   return $ Abstract (Pos i)
+--
+-- addToContext :: Var -> Prop -> EnvM ()
+-- addToContext var prop = modify $ \ st -> st { stContext = Map.insert var prop (stContext st) }
 
 --------------------------------------------------------------------------------
 -- |
@@ -76,7 +69,6 @@ addToContext var prop = modify $ \ st -> st { stContext = Map.insert var prop (s
 --   ctx <- gets stContext
 --   unless (Set.null ctx) $ throwError ContextShouldBeEmpty
 
---                   | Compose   (TermName ann) (Process  ann) (Process ann)   ann
 --                   | Output    (TermName ann) (TermName ann) (Process ann) (Process ann) ann
 --                   | Input     (TermName ann) (TermName ann) (Process ann)   ann
 --                   | SelectL   (TermName ann) (Process  ann)                 ann
@@ -88,63 +80,74 @@ addToContext var prop = modify $ \ st -> st { stContext = Map.insert var prop (s
 --                   | EmptyInput               (TermName ann) (Process ann)   ann
 --                   | EmptyChoice
 
-lookup :: Var -> EnvM Prop
-lookup var = do
-  ctx <- gets stContext
-  case Map.lookup var ctx of
-    Nothing -> throwError $ NotInContext var
+
+infer :: Term -> EnvM Context
+infer (C.Link x y _) = do
+  t <- lookupType x
+  u <- lookupType y
+  (t', u') <- unifyOpposite t u
+  return $ Map.fromList
+    [ (x, t')
+    , (y, u')
+    ]
+infer (C.Compose x p q _) = do
+  a <- Map.delete x <$> infer p
+  b <- Map.delete x <$> infer q
+  checkOverlappedContext a b
+  return $ mergeContext a b
+-- infer (C.Output x y p q _) = do
+--   a <- infer p
+--   b <- infer q
+
+
+-- unsafe, does not check if contexts are overlapping
+mergeContext :: Context -> Context -> Context
+mergeContext = Map.merge
+  (Map.mapMaybeMissing (\_ x -> Just x))
+  (Map.mapMaybeMissing (\_ x -> Just x))
+  (Map.zipWithMaybeMatched (\_ x _ -> Just x))
+
+lookupType :: Var -> EnvM Type
+lookupType var = do
+  types <- gets stTypes
+  case Map.lookup var types of
+    Nothing -> throwError $ VarNotFoundInTypes var
     Just t -> return t
 
 -- substitute type variables with concrete types or other type variables
-substitute :: Index -> Prop -> EnvM ()
-substitute i t = do
-  ctx <- gets stContext
-  putContext $ fmap subst ctx
+substituteType :: Index -> Type -> EnvM ()
+substituteType i t = do
+  types <- gets stTypes
+  putTypes    $ fmap substType types
+  ctxs <- gets stContexts
+  putContexts $ fmap substCtx  ctxs
+
   where
-    subst :: Prop -> Prop
-    subst (Abstract j) = if i == j then t else Abstract j
-    subst others = others
+    substType :: Type -> Type
+    substType (Var j) = if i == j then t else Var j
+    substType others = others
 
-unifyOpposite :: Prop -> Prop -> EnvM ()
-unifyOpposite (Concrete t) (Concrete u) = unless (t == dual u) $
-  throwError $ CannotUnify t u
-unifyOpposite (Concrete t) (Abstract u) = substitute u (Concrete (dual t))
-unifyOpposite (Abstract t) (Concrete u) = substitute t (Concrete (dual u))
-unifyOpposite (Abstract t) (Abstract u) = substitute t (Abstract (dual u))
+    substCtx :: Context -> Context
+    substCtx ctx = fmap substType ctx
 
--- instantiate a new type variable
-assume :: Var -> EnvM Prop
-assume var = do
-  ctx <- gets stContext
-  when (Map.member var ctx) $
-    throwError $ VarNotFresh var Nothing
-  i <- getFreshTypeVar
-  addToContext var i
-  return i
+unifyOpposite :: Type -> Type -> EnvM (Type, Type)
+unifyOpposite (Var i) (Var j) = do
+  let i' = Var (dual i)
+  -- replace j with the opposite of i
+  substituteType j i'
+  return (Var i, i')
+unifyOpposite (Var i) u = do
+  substituteType i (dual u)
+  return (dual u, u)
+unifyOpposite t (Var i) = do
+  substituteType i (dual t)
+  return (t, dual t)
+unifyOpposite t u = if t == dual u
+  then throwError $ CannotUnify t u
+  else return (t, u)
 
-infer :: Term -> EnvM ()
-infer (Link x y _) = do
-  t <- lookup x
-  u <- lookup y
-  unifyOpposite t u
-infer (Compose x p q _) = do
-  t <- assume x
-  infer
-
-  -- infer p
-
-
-  -- gets
-  -- putContext
-  -- | Compose   (TermName ann) (Process  ann) (Process ann)   ann
-  -- | Output    (TermName ann) (TermName ann) (Process ann) (Process ann) ann
-  -- | Input     (TermName ann) (TermName ann) (Process ann)   ann
-  -- | SelectL   (TermName ann) (Process  ann)                 ann
-  -- | SelectR   (TermName ann) (Process  ann)                 ann
-  -- | Choice    (TermName ann) (Process  ann) (Process ann)   ann
-  -- | Accept    (TermName ann) (TermName ann) (Process ann)   ann
-  -- | Request   (TermName ann) (TermName ann) (Process ann)   ann
-  -- | EmptyOutput              (TermName ann)                 ann
-  -- | EmptyInput               (TermName ann) (Process ann)   ann
-  -- | EmptyChoice              (TermName ann)                 ann
-  -- deriving (Show, Functor)
+checkOverlappedContext :: Context -> Context -> EnvM ()
+checkOverlappedContext a b = do
+  let overlapped = Map.difference a b
+  unless (Map.null overlapped) $
+    throwError $ OverlappedContext overlapped
