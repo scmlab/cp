bd790f84d571a0ed98e2efffd847b3379e8e0bd1
LUA Ting-Gan <banacorn@gmail.com>
2019-05-13 12:49:01 +0800

    tests for type inference

--- test/Test/TypeChecking.hs
+++ test/Test/TypeChecking.hs
@@ -18,11 +18,125 @@ import TypeChecking (inferTerm)
 import Main
 
 tests :: TestTree
-tests = testGroup "TypeChecker"
-  [ emptyOutput
+tests = testGroup "Type Inference"
+  [ basics
+  ]
+
+
+basics :: TestTree
+basics = testGroup "basics"
+  [ link
+  , cut
+  , output
+  , input
+  , selectLeft
+  , selectRight
+  , choice
+  , accept
+  , request
+  , outputType
+  , inputType
+  , emptyOutput
   , emptyInput
   , emptyChoice
   ]
+
+link :: TestTree
+link = testCase "link" $ do
+  actual <- parseProc "x <-> y" >>= infer
+  let expected = Map.fromList
+        [ ("x", Dual (Var (Nameless 1)))
+        , ("y",      (Var (Nameless 1)))
+        ]
+  actual @?= expected
+
+cut :: TestTree
+cut = testCase "cut" $ do
+  actual <- parseProc "\\x . (x[].end | x().end)" >>= infer
+  let expected = Map.fromList []
+  actual @?= expected
+
+
+output :: TestTree
+output = testCase "output" $ do
+  actual <- parseProc "x[y].(y[].end | x().end)" >>= infer
+  let expected = Map.fromList [("x", Times One Bot)]
+  actual @?= expected
+
+
+input :: TestTree
+input = testCase "input" $ do
+  actual <- parseProc "x(y).(y[].end)" >>= infer
+  let expected = Map.fromList
+        [ ("x", Par One (Var (Nameless 1)))
+        ]
+  actual @?= expected
+
+selectLeft :: TestTree
+selectLeft = testCase "select left" $ do
+  actual <- parseProc "x[inl].x[].end" >>= infer
+  let expected = Map.fromList
+        [ ("x", Plus One (Var (Nameless 1)))
+        ]
+  actual @?= expected
+
+selectRight :: TestTree
+selectRight = testCase "select right" $ do
+  actual <- parseProc "x[inr].x[].end" >>= infer
+  let expected = Map.fromList
+        [ ("x", Plus (Var (Nameless 1)) One)
+        ]
+  actual @?= expected
+
+choice :: TestTree
+choice = testCase "choice" $ do
+  actual <- parseProc "x.case(x[].end, x[].end)" >>= infer
+  let expected = Map.fromList
+        [ ("x", With One One)
+        ]
+  actual @?= expected
+
+
+accept :: TestTree
+accept = testCase "accept" $ do
+  actual <- parseProc "!x(y).(y[].end)" >>= infer
+  let expected = Map.fromList
+        [ ("x", Acc One)
+        ]
+  actual @?= expected
+
+request :: TestTree
+request = testCase "request" $ do
+  actual <- parseProc "?x[y].y[].end" >>= infer
+  let expected = Map.fromList
+        [ ("x", Req One)
+        ]
+  actual @?= expected
+--
+-- weaken :: TestTree
+-- weaken = testCase "weaken" $ do
+--   actual <- parseProc "x[].end" >>= infer
+--   let expected = Map.fromList
+--         [ ("x", One)
+--         , ("y", Req One)
+--         ]
+--   actual @?= expected
+
+outputType :: TestTree
+outputType = testCase "output type" $ do
+  actual <- parseProc "x[1].x[].end" >>= infer
+  let expected = Map.fromList
+        [ ("x", Exists Unknown (Var (Nameless 1)) (Just (One, One)))
+        ]
+  actual @?= expected
+
+inputType :: TestTree
+inputType = testCase "input type" $ do
+  actual <- parseProc "x(X).x[].end" >>= infer
+  let expected = Map.fromList
+        [ ("x", Forall (Named "X") One)
+        ]
+  actual @?= expected
 
 emptyOutput :: TestTree
 emptyOutput = testCase "emptyOutput" $ do
